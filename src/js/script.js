434941d969d3019772c0dd4c8ae2012fad844905
emma <egujral26@aoiths.org>
2026-01-18 22:23:29 -0500

    added colleges and info

---
+++ src/js/script.js
@@ -0,0 +1,53 @@
+const colleges = [
+    {
+        id: harvard,
+        info: {
+            location:"Cambridge, Massachusetts",
+            program:"Mind, Brain, and behavior program",
+            website:"https://www.harvard.edu",
+            logo:"https://upload.wikimedia.org/wikipedia/commons/c/cc/Harvard_University_coat_of_arms.svg",
+            rank:"1"
+        }
+    },
+    {
+        id: princeton,
+        info: {
+            location:"Princeton, New Jersey",
+            program:"Computer Science/Psychology",
+            website:"https://www.princeton.edu",
+            logo:"https://upload.wikimedia.org/wikipedia/commons/d/d0/Princeton_seal.svg",
+            rank:"2"
+        }
+    },
+    {
+        id: "SUNY Binghamton",
+        info: {
+            location: "Binghamton, New York",
+            program: "computer science",
+            website: "https://www.binghamton.edu",
+            logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRaZUgiT-j8HVNs_ieiw1QGJPa4qkWQ5UZtg&s",
+            rank: "3"
+        }
+    },
+    {
+        id: "SUNY Stonybrook",
+        info: {
+            location: "Stonybrook, New York",
+            program: "computer science",
+            website: "https://stonybrook.edu",
+            logo: "https://www.stonybrook.edu/marcom/_images/2015/10/logo_stacked_vert.jpg",
+            rank: "4"
+        }
+    },
+    {
+        id: "New York University",
+        info: {
+            location: "Manhattan, New York",
+            program: "computer science",
+            website: "https://www.nyu.edu",
+            logo: "https://fundit.fr/sites/default/files/styles/max_650x650/public/actors/2518-universite-new-york-nyu.png?itok=z-UBR5U8",
+            rank: "5"
+        }
+    }
+
+]
